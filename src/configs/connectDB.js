92ea81617e0d0c7be3dfa07452c2bf0524d57cb6
hung <hungvuhunglinh@gmail.com>
2022-04-18 22:45:07 +0700

    update signin +1

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -4,9 +4,9 @@ import mysql from 'mysql2/promise';
 console.log("Creating connection pool...");
 const pool = mysql.createPool({
     host: 'remotemysql.com',
-    user: '6FgoF2iQ6r',
-    database: '6FgoF2iQ6r',
-    password: 'ZOhvm8yeew',
+    user: 'ZaSSRc4FOj',
+    database: 'ZaSSRc4FOj',
+    password: '8axxIxV5hV',
     port: '3306'
 });
 export default pool;
