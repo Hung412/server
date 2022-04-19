e6bfcdd5b601614f2af9e76910fa2036affba1b2
hung <hungvuhunglinh@gmail.com>
2022-04-19 23:21:02 +0700

    signin 19-4 v1.6

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -17,7 +17,7 @@ const postDeleteStatus = async (req, res) => {
 
 const signin = async (req, res) => {
     await pool.execute('SELECT * FROM `user`');
-    if(req.body.uname == 'admin'){
+    if(req.body.uname == "admin"){
         return res.render('controllPanel.ejs');
     }
     else{
