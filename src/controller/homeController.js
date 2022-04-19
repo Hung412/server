5e73de09d90eef2b0ceeb3a0099a8467e71b088b
hung <hungvuhunglinh@gmail.com>
2022-04-19 22:06:50 +0700

    test signin

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -16,7 +16,7 @@ const postDeleteStatus = async (req, res) => {
 }
 
 const signin = (req, res) => {
-    const [rows, fields] = await pool.execute('SELECT * FROM `user`');
+    //const [rows, fields] = await pool.execute('SELECT * FROM `user`');
     // if(fields.quyenhan == 1){
     //     return res.render('controllPanel.ejs');
     // }
