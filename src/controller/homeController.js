455751555be14594a9cb40f629190c9270511da6
hung <hungvuhunglinh@gmail.com>
2022-04-21 08:15:50 +0700

    signin 21-4 v1.0

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -16,14 +16,8 @@ const postDeleteStatus = async (req, res) => {
 }
 
 const signin = async (req, res) => {
-    const [rows, fields] = await pool.execute(`SELECT * FROM user`);
-    res.render('signin.ejs');
-    if(req.body.uname == rows[0].username){
-        alert("OK!")
-    }
-    else{
-        alert("Again!")
-    }     
+    await pool.execute(`SELECT * FROM user WHERE username = ${req.body.uname} AND password = ${req.body.psw}`);
+    return res.render('signin.ejs');
 }
 
 module.exports = {
