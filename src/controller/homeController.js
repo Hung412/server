77b7d41aa111b0cb64045ebbe3cc9ca1d5a2303c
hung <hungvuhunglinh@gmail.com>
2022-04-21 10:48:54 +0700

    login ok 21-4

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,7 @@
 import pool from '../configs/connectDB';
 
 const getHomePage = async (req, res) => {
-    const [rows, fields] = await pool.execute('SELECT * FROM `user`');
+    const [rows, fields] = await pool.execute('SELECT * FROM `trangthai`');
     return res.render('index.ejs', { data: rows });
 }
 
