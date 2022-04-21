8f27720aff602782e3ae71ddcc53278bfa7ab61a
hung <hungvuhunglinh@gmail.com>
2022-04-21 09:29:51 +0700

    signin 21-4 v1.21

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,7 @@ let router = express.Router();
 
 const initWebRouter = (app) => {
     router.get('/', homeController.getHomePage);
-    router.get('/home', homeController.signin);
+    router.get('/home', homeController.getHomePage);
     router.get('/controll-panel', homeController.signin);
     router.post('/delete-status', homeController.postDeleteStatus);
     router.get('/sign-in', homeController.signin);
