24b68911f97ddd4d14ec0de91bcb74e71ce96d51
hung <hungvuhunglinh@gmail.com>
2022-04-21 10:40:58 +0700

    signin 21-4 v1.29.6

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,7 @@
 import pool from '../configs/connectDB';
 
 const getHomePage = async (req, res) => {
-    const [rows, fields] = await pool.execute('SELECT * FROM `trangthai`');
+    const [rows, fields] = await pool.execute('SELECT * FROM `user`');
     return res.render('index.ejs', { data: rows });
 }
 
