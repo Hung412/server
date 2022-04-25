bc2bdc854bd4f5610c4b7c446232d5b1045ddc57
hung <hungvuhunglinh@gmail.com>
2022-04-25 21:36:42 +0700

    clock 25-4 v1.8

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -24,8 +24,8 @@ const signin = async (req, res) => {
         if(req.body.username == rows[i].username && req.body.password == rows[i].password){
             return res.redirect('/controll-panel');
         }else{
-            alert("Please try again!")
-            return res.redirect('/sign-in')
+            alert("Please try again!");
+            return res.redirect('/sign-in');
         }
     }
 }
