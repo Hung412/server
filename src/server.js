f8080596596dd91c75f268d6cfaa2bad97b4af10
hung <hungvuhunglinh@gmail.com>
2022-05-22 21:28:15 +0700

    22-5 v.1

--- src/server.js
+++ src/server.js
@@ -46,47 +46,10 @@ ws.on('connection', function (socket, req, res) {
     clients.push(socket);
 
     socket.on('message', function (message) {
-            
-        broadcast(socket, message);
+        const user = JSON.parse(message);
+        broadcast(socket, user.id);
 
-        console.log('Message: %s', message);
-        if (message == "open successfully" && temp == 0) {
-
-            con.query("INSERT INTO trangthai (status) VALUES ('open')", function (err, result) {
-
-                if (err) throw err;
-
-                console.log('Insert data open successfully');
-            });
-
-            temp = 1;
-            tempError = 1;
-
-        }
-        else if (message == "close successfully" && temp == 1) {
-
-            con.query("INSERT INTO trangthai (status) VALUES ('close')", function (err, result) {
-
-                if (err) throw err;
-
-                console.log('Insert data close successfully');
-            });
-
-            temp = 0;
-            tempError = 1; 
-        }
-        else if ((message == "open error" || message == "close error") && tempError == 1) {
-
-            con.query("INSERT INTO trangthai (status) VALUES ('error')", function (err, result) {
-
-                if (err) throw err;
-
-                console.log('Insert data error successfully');
-            }); 
-
-            tempError = 0;
-        }
-
+        console.log('Message: %s', user.id);
     });
 
     socket.on('close', function () {
