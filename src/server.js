ac094db98e6195911b833eef52a6bdda25ad976f
hung <hungvuhunglinh@gmail.com>
2022-05-23 00:15:08 +0700

    22-5 v.1.18

--- src/server.js
+++ src/server.js
@@ -47,8 +47,8 @@ ws.on('connection', function (socket, req, res) {
     clients.push(socket);
     var [rows, fields] = pool.execute(`SELECT * FROM nguoidung`);
     socket.on('message', function (message) {
-        for(let i=0; i<=10; i++){
-            if(message == rows[i].name){
+        for(let i=0; i<=rows.length; i++){
+            if(rows[i].name == message){
                 broadcast(socket, message);
                 console.log('Message: %s', message);
             }else{
