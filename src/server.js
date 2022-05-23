96a02de8f1dc946eb0386108c866b36e96a86a09
hung <hungvuhunglinh@gmail.com>
2022-05-24 02:11:12 +0700

    24-5 v22

--- src/server.js
+++ src/server.js
@@ -46,16 +46,16 @@ ws.on('connection', function (socket, req, res) {
 
     clients.push(socket);
     socket.on('message', function (message) {
-        const rows = pool.execute(`SELECT * FROM nguoidung`);
-        for(let i=0; i<rows.length; i++){
-            if(rows[i].name == message){
-                broadcast(socket, message);
-                console.log('Recognition: %s', message);
+        // const rows = pool.execute(`SELECT * FROM nguoidung`);
+        // for(let i=0; i<rows.length; i++){
+        //     if(rows[i].name == message){
+        //         broadcast(socket, message);
+        //         console.log('Recognition: %s', message);
 
-            }
-        }
-        // broadcast(socket, mess);
-        // console.log('Message: %s', mess);
+        //     }
+        // }
+        broadcast(socket, message);
+        console.log('Message: %s', message);
     });
 
     socket.on('close', function () {
