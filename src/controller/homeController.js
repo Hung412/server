713e86aefc3b2ade3ea06f8ec443416b40eac15c
hung <hungvuhunglinh@gmail.com>
2022-05-23 21:32:37 +0700

    23-5 v3

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -34,28 +34,26 @@ const signin = async (req, res) => {
     console.log(rows, rows.length)
     for(let i=0; i<=rows.length; i++){
         console.log(rows[i].username);
+        // if(req.body.username == rows[i].username && req.body.password == rows[i].password){
+        //     var sess = req.session;  //initialize session variable
+        //     sess.daDangNhap = true;
+        //     sess.username = req.body.username; 
+        //     return res.redirect('/controll-panel');
+        // }else{
+        //     return res.redirect('/sign-in'); 
+        // }
         if(req.body.username == rows[i].username && req.body.password == rows[i].password){
             var sess = req.session;  //initialize session variable
             sess.daDangNhap = true;
             sess.username = req.body.username; 
+        }
+        if (req.session.daDangNhap){
             return res.redirect('/controll-panel');
-        }else{
+        }
+        else{
             return res.redirect('/sign-in'); 
         }
-        // if(req.body.username == rows[i].username && req.body.password == rows[i].password){
-        //     var sess = req.session;  //initialize session variable
-        //         sess.daDangNhap = true;
-        //         sess.username = req.body.username; 
-        // }
     }
-
-    // if (req.session.daDangNhap){
-    //     return res.redirect('/controll-panel');
-    // }
-    // else{
-    //     return res.redirect('/sign-in'); 
-    // }
-
 }
 
 module.exports = {
