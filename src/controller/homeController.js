04aba7a7d47701d8e1db58709deaf7d78ff93506
hung <hungvuhunglinh@gmail.com>
2022-05-23 21:26:24 +0700

    23-5 v3

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -33,7 +33,7 @@ const signin = async (req, res) => {
     const [rows, fields]  = await pool.execute(`SELECT * FROM user`)
     console.log(rows, rows.length)
     for(let i=0; i<=rows.length; i++){
-        console.log(rows[i].username);
+        // console.log(rows[i].username);
         // if(req.body.username == rows[i].username && req.body.password == rows[i].password){
         //     var sess = req.session;  //initialize session variable
         //     sess.daDangNhap = true;
