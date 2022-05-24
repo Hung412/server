cb18fed98641b1d7ac8caba76b372206f76babc9
hung <hungvuhunglinh@gmail.com>
2022-05-25 02:06:49 +0700

    24-5 v22

--- src/server.js
+++ src/server.js
@@ -43,46 +43,30 @@ function broadcast(socket, data) {
 }
 
 ws.on('connection', function (socket, req, res) {
-    const get_data = async() =>{
-        const [rows, fields]  = await pool.execute(`SELECT * FROM nguoidung`);
-        const face = [];
-        // console.log(rows);
-        for(let i=0; i<rows.length; i++){
-            face.push(rows[i].name);
-        }
-        console.log(face);
-        // return face;
-    }
     clients.push(socket);
-    //const a = "";
     socket.on('message', function (message) {
         const get_data = async() =>{
             const [rows, fields]  = await pool.execute(`SELECT * FROM nguoidung`);
             const face = [];
-            // console.log(rows);
             for(let i=0; i<rows.length; i++){
                 face.push(rows[i].name);
             }
             console.log(face);
-            // return face;
             console.log('Message: %s', message);
             if(message=="close successfully" || message=="close error"){
                 broadcast(socket, "CLOSE CONFIRM");
             }
-            // if(message=="DinhVanKhoa"){
-            //     broadcast(socket, "FACE_RECOGNITION_CONFIRM");
-            // }else{
-            //     broadcast(socket, message);
-            // }
-            // get_data();
+            
             for(let i=0; i<face.length; i++){
                 if(message == face[i]){
                     console.log('Recognition: %s', message);
+                    broadcast(socket, "FACE_RECOGNITION_CONFIRM");
+                }else{
+                    broadcast(socket, message);
                 }
             }
         }
         get_data();
-        
     });
 
     socket.on('close', function () {
