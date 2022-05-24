f57140a8a421b79fee56cd6bc0b68809785281c4
hung <hungvuhunglinh@gmail.com>
2022-05-24 21:19:21 +0700

    24-5 v2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -5,7 +5,7 @@ const getHomePage = async (req, res) => {
     const count = await pool.execute('SELECT COUNT(*) AS `countopen` FROM `trangthai` WHERE status = "open"');
     if (req.session.daDangNhap) {
         console.log(req.session.username);
-        console.log(count);
+        console.log(count.countopen);
         return res.render('index.ejs', { data: rows });
     }
     else {       
