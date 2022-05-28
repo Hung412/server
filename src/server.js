a502903ce9bcf64ec2c828993b46ed001cdb0aba
hung <hungvuhunglinh@gmail.com>
2022-05-29 00:40:19 +0700

    28-5

--- src/server.js
+++ src/server.js
@@ -51,12 +51,16 @@ ws.on('connection', function (socket, req, res) {
             const face = [];
             for(let i=0; i<rows.length; i++){
                 face.push(rows[i].name);
+                if(message.length == 22 && rows[i].quyenhan == 0){
+                    console.log(message);
+                }
             }
             console.log(face);
             console.log('Message: %s', message);
             if(message=="close successfully" || message=="close error"){
                 broadcast(socket, "CLOSE CONFIRM");
             }
+            if(message.length == 22 && rows[i])
             for(let i=0; i<face.length; i++){
                 if(message == face[i] && rows[i].quyenhan == 1){
                     console.log('Recognition: %s', message);
