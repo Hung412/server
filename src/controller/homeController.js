4273846481c16beff467481b0d86bbb30c89d511
hung <hungvuhunglinh@gmail.com>
2022-05-30 08:34:26 +0700

    30-5

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,12 @@
 import pool from '../configs/connectDB';
 
 const getHomePage = async (req, res) => {
-    // const [rows, fields] = await pool.execute('SELECT * FROM `trangthai`');
-    const [count, fields] = await pool.execute('SELECT COUNT(*) AS `count` FROM `trangthai` WHERE status = "open" UNION SELECT COUNT(*) AS `countclose` FROM `trangthai` WHERE status = "close" UNION SELECT COUNT(*) AS `counterror` FROM `trangthai` WHERE status = "error"');
+    const [rows, fields] = await pool.execute('SELECT * FROM `trangthai` ORDER BY `timestatus` DESC');
+    // const [count, fields] = await pool.execute('SELECT COUNT(*) AS `count` FROM `trangthai` WHERE status = "open" UNION SELECT COUNT(*) AS `countclose` FROM `trangthai` WHERE status = "close" UNION SELECT COUNT(*) AS `counterror` FROM `trangthai` WHERE status = "error"');
     if (req.session.daDangNhap) {
         console.log(req.session.username);
         // console.log(count);
-        return res.render('index.ejs', { data: count });
+        return res.render('index.ejs', { data: rows });
     }
     else {       
         res.redirect("/sign-in");
