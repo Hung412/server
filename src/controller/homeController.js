aad2038449669ef5f52b1a84fed89dbf75eb8916
hung <hungvuhunglinh@gmail.com>
2022-05-30 16:53:48 +0700

    30-5

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -40,7 +40,8 @@ const getControllPanelPage = (req, res) => {
 
 const postDeleteStatus = async (req, res) => {
     console.log("Check request: ", req.body);
-    const [rows, fields] = await pool.execute(`SELECT * FROM trangthai WHERE timestatus BETWEEN CURRENT_DATE - 2 AND CURRENT_DATE`)
+    const [rows, fields] = await pool.execute(`SELECT * FROM trangthai WHERE timestatus BETWEEN CURRENT_DATE - 2 AND CURRENT_DATE`);
+    console.log(rows);
     return res.render('homedata.ejs', { data: rows });
 }
 const showSigninForm = async (req, res) => {
