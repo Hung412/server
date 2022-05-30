de4a86821295ff82594535c56af28b1be3591d55
hung <hungvuhunglinh@gmail.com>
2022-05-30 10:51:54 +0700

    30-5

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,27 +1,85 @@
 import pool from '../configs/connectDB';
 
-const getHomePage = async (req, res) => {
-    const [rows, fields] = await pool.execute('SELECT * FROM `trangthai` ORDER BY `timestatus` DESC');
-    // const [count, fields] = await pool.execute('SELECT COUNT(*) AS `count` FROM `trangthai` WHERE status = "open" UNION SELECT COUNT(*) AS `countclose` FROM `trangthai` WHERE status = "close" UNION SELECT COUNT(*) AS `counterror` FROM `trangthai` WHERE status = "error"');
-    if (req.session.daDangNhap) {
-        console.log(req.session.fullname);
-        return res.render('index.ejs', { data: rows });
-    }
-    else {       
-        res.redirect("/sign-in");
-    }
-}
+// const getHomePage = async (req, res) => {
+//     const [rows, fields] = await pool.execute('SELECT * FROM `trangthai` ORDER BY `timestatus` DESC');
+//     // const [count, fields] = await pool.execute('SELECT COUNT(*) AS `count` FROM `trangthai` WHERE status = "open" UNION SELECT COUNT(*) AS `countclose` FROM `trangthai` WHERE status = "close" UNION SELECT COUNT(*) AS `counterror` FROM `trangthai` WHERE status = "error"');
+//     if (req.session.daDangNhap) {
+//         console.log(req.session.fullname);
+//         return res.render('index.ejs', { data: rows });
+//     }
+//     else {       
+//         res.redirect("/sign-in");
+//     }
+// }
 
-const getCount = async(req, res) =>{
+const getHomePage = async(req, res) =>{
     const [count, fields] = await pool.execute('SELECT COUNT(*) AS `countstatus` FROM `trangthai` WHERE status = "open" UNION SELECT COUNT(*) AS `countclose` FROM `trangthai` WHERE status = "close" UNION SELECT COUNT(*) AS `counterror` FROM `trangthai` WHERE status = "error"');
-    // const countstatus = [];
+    const countstatus = [];
     if (req.session.daDangNhap) {
         //Chart
-        // for(let i=0; i<count.length; i++){
-        //     countstatus.push(count[i].countstatus);
-        // }
+        for(let i=0; i<count.length; i++){
+            countstatus.push(count[i].countstatus);
+        }
+        let myChart = document.getElementById('myChart').getContext('2d');
+        // Global Options
+        Chart.defaults.global.defaultFontFamily = 'Lato';
+        Chart.defaults.global.defaultFontSize = 18;
+        Chart.defaults.global.defaultFontColor = '#777';
+        let massPopChart = new Chart(myChart, {
+        type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data:{
+            labels:['Open', 'Close', 'Error'],
+            datasets:[{
+            label:'Status',
+            data:[
+                countstatus[0],
+                countstatus[1],
+                countstatus[2],
+            ],
+            //backgroundColor:'green',
+            backgroundColor:[
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
+                'rgba(255, 159, 64, 0.6)',
+                'rgba(255, 99, 132, 0.6)'
+            ],
+            borderWidth:1,
+            borderColor:'#777',
+            hoverBorderWidth:3,
+            hoverBorderColor:'#000'
+            }]
+        },
+        options:{
+            title:{
+            display:true,
+            text:'Status Chart',
+            fontSize:25
+            },
+            legend:{
+            display:true,
+            position:'right',
+            labels:{
+                fontColor:'#000'
+            }
+            },
+            layout:{
+            padding:{
+                left:50,
+                right:0,
+                bottom:0,
+                top:0
+            }
+            },
+            tooltips:{
+            enabled:true
+            }
+        }
+        });
         //end chart
-        console.log(count);
+        console.log(countstatus);
         console.log(req.session.fullname);
         return res.render('index.ejs', { data: count });
     }
